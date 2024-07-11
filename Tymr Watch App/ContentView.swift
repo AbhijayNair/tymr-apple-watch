de56c413b79bdc93f96e97a561196f62afa3ad3a
Jay Nair <abhijaynair@icloud.com>
2024-07-11 13:55:47 -0400

    Initial Commit

---
+++ Tymr Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tymr Watch App
+//
+//  Created by Abhijay Nair on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tymr Watch App/TymrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymrApp.swift
+//  Tymr Watch App
+//
+//  Created by Abhijay Nair on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Tymr_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
